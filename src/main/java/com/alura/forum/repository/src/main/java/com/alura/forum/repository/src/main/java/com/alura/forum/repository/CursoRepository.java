b233171ca65f3569376c0010b5722b589a6618a7
AleCezar24 <alejandrocpc5000@gmail.com>
2025-07-24 12:48:43 -0300

    Create CursoRepository.java

---
+++ src/main/java/com/alura/forum/repository/src/main/java/com/alura/forum/repository/src/main/java/com/alura/forum/repository/CursoRepository.java
@@ -0,0 +1,12 @@
+package com.alura.forum.repository;
+
+import com.alura.forum.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+    Optional<Curso> findByNome(String nome);
+}
