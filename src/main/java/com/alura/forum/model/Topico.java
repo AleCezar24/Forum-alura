7adb60e27ef3c9f3fc35c738fa994d8e139b801d
AleCezar24 <alejandrocpc5000@gmail.com>
2025-07-24 12:46:06 -0300

    Create Topico.java

---
+++ src/main/java/com/alura/forum/model/Topico.java
@@ -0,0 +1,79 @@
+package com.alura.forum.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
+
+    @ManyToOne
+    private Usuario autor;
+
+    @ManyToOne
+    private Curso curso;
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public StatusTopico getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusTopico status) {
+        this.status = status;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
